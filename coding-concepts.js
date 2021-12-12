103402a027d04bfbfa6d767439dfca07909f4080
Echo5Mike <1raul.marquez@gmail.com>
2021-12-11 19:04:01 -0800

    done with coding-concetpts

--- coding-concepts.js
+++ coding-concepts.js
@@ -10,8 +10,8 @@
 var cohort = "Echo 2021"
 // console.log(cohort.split(""))
 
-// a) Your answer:
-// b) Verify and explain:
+a) Your answer: I belive that . split will return an array ["E", "c"......]
+b) Verify and explain: It did return an array with each value in single qoutes and a comma 
 
 
 // --------------------2) What will this log?
@@ -21,8 +21,9 @@ const greeter = (name) => {
 }
 // console.log(greeter("LEARN Student"))
 
-// a) Your answer:
-// b) Verify and explain:
+a) Your answer: when const greeter is invoked and "LEARN Student" get passed in as an agurment, it will "replace" anywhere where (name) is
+
+b) Verify and explain: Oh you got me, undefined, there is no return 
 
 
 // --------------------3) What will this log?
@@ -30,8 +31,10 @@ const greeter = (name) => {
 var multBy2 = [4, 5, 6, 7, 8].map(value => value * 2)
 // console.log(multBy2)
 
-// a) Your answer:
-// b) Verify and explain:
+a) Your answer: I don't remember if HOF need a return but if it doesn't it will return an array of the same length and each value will be multiplied by 3.
+
+b) Verify and explain: Indeed, HOF doesnt need a return answer is correct.
+
 
 
 // --------------------4) What will this log?
@@ -39,8 +42,10 @@ var multBy2 = [4, 5, 6, 7, 8].map(value => value * 2)
 var onlyOdds = [11, 12, 13, 14, 15].filter(value => value % 2 !== 0)
 // console.log(onlyOdds)
 
-// a) Your answer:
-// b) Verify and explain:
+a) Your answer:It will return an array with the values that are NOT even
+
+b) Verify and explain:Correct it returned and array with [ 11, 13, 15 ] which are not even
+
 
 
 // --------------------5) What will this log?
@@ -53,8 +58,9 @@ var myCodingSkills = {
 }
 // console.log(myCodingSkills.languages[0])
 
-// a) Your answer:
-// b) Verify and explain:
+a) Your answer: It return "JS" since it is in the index of 0 in the languages array.
+
+b) Verify and explain: correct it returned JS
 
 
 // --------------------STRETCH: What will this log?
@@ -69,5 +75,6 @@ class Learn {
 var learnStudent = new Learn("George")
 // console.log(learnStudent)
 
-// a) Your answer:
-// b) Verify and explain:
+a) Your answer:I think, it will pass in the argument of geroge into the class learn, and return george delta 2021
+
+b) Verify and explain: It returned the key and value pair while passing in the agurment of george.
